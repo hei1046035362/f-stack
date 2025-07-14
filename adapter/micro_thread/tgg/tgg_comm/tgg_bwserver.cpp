277b25bd575b454a300675028effc3aefca2c6a1
hepeng <hepeng@teamgaga.com>
2025-07-14 17:25:53 +0800

    [fix]调整rapid数组添加成员的方式，优化array_replace_recursive的性能并完善updatesession接口

--- adapter/micro_thread/tgg/tgg_comm/tgg_bwserver.cpp
+++ adapter/micro_thread/tgg/tgg_comm/tgg_bwserver.cpp
@@ -337,7 +337,8 @@ void *read_routine( void *arg )
                 // 验证数据包长度有效性[4](@ref)
                 if(pack_len < sizeof(tgg_bw_protocal) || 
                    pack_len > MAX_PACKET_SIZE) {
-                    LOG_ERROR("invalid packet len, bw[ip:%s,port%d] is closing.", ip_str, ntohs(port));
+                    LOG_ERROR("invalid packet len[%d], bw[ip:%s,port%d] is closing.", pack_len, ip_str, ntohs(port));
+                    LOG_ERROR("bin data:%s.", bin2hex(recv_buffer).c_str());
                     tgg_close_bw_session(g_prc_id, fd);
                     close( fd );
                     LOG_ERROR("bw[ip:%s,port%d] closed.", ip_str, ntohs(port));
