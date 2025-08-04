5bebfdf015876717de9fda557eafbbe233940ebb
hepeng <hepeng@teamgaga.com>
2025-08-04 17:33:58 +0800

    [fix]修复部分程序绑核问题

--- adapter/micro_thread/tgg/cliprc.c
+++ adapter/micro_thread/tgg/cliprc.c
@@ -61,7 +61,7 @@ void tgg_process_uninit()
 static void prc_dpdk_eal_init(int argc, char **argv)
 {
 	char c_flag[24] = {0};
-	sprintf(c_flag, "-c%d", TggConfigure::getInstance()->get_ccore_mask());
+	sprintf(c_flag, "-c%x", TggConfigure::getInstance()->get_ccore_mask());
 	char n_flag[] = "-n4";
 	char mp_flag[] = "--proc-type=secondary";
 	char log_flag[] = "--log-level=6";
