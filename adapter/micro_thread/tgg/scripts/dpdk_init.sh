33b689ff49a2032f12e98834f1bc8e1c521eee0e
hepeng <hepeng@teamgaga.com>
2025-08-08 18:13:45 +0800

    [add]加入服务管理

--- adapter/micro_thread/tgg/scripts/dpdk_init.sh
+++ adapter/micro_thread/tgg/scripts/dpdk_init.sh
@@ -1,17 +1,61 @@
 # 分配大页  根据实际情况调整大小
-echo 1536 > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-mkdir /mnt/huge
-mount -t hugetlbfs nodev /mnt/huge
-echo 0 > /proc/sys/kernel/randomize_va_space
+
+# 获取CPU核心数（物理核心）
+get_cpu_cores() {
+    # if command -v lscpu &> /dev/null; then
+    #     lscpu | awk -F: '/^CPU\(s\):/ {gsub(/[^0-9]/, "", $2); print $2}' 
+    # else
+    grep -c "cpu cores" /proc/cpuinfo
+    # fi
+}
+
+# 计算大页页数
+calc_hugepages() {
+    local cores=$(get_cpu_cores)  # 核数×3/4
+    echo $(( cores * 3 / 4 * 512 ))                    # 转换为2MB页数
+}
+
+# 设置大页内存
+set_hugepages() {
+    local cur_huge=$(calc_hugepages)
+    local prev_huges=$(cat /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages)
+    if [ "$cur_huge" -ne "$prev_huges" ]; then
+        echo "大页内存($prev_huges)不够即将设置为 $cur_huge"
+        echo $cur_huge > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+        mkdir /mnt/huge
+        mount -t hugetlbfs nodev /mnt/huge
+        echo 0 > /proc/sys/kernel/randomize_va_space
+    fi
+}
 
 # 加载驱动
-modprobe uio
-insmod /usr/src/linux-headers-$(uname -r)/extra/dpdk/igb_uio.ko
-insmod /usr/lib/modules/$(uname -r)/extra/dpdk/rte_kni.ko carrier=on
-
-# 网口驱动更换(内核驱动换igb_uio)  
+insmod_dpdk_io() {
+    if lsmod | grep -q "^igb_uio"; then
+        echo "igb_uio 模块已加载"
+    else
+        echo "igb_uio 驱动尝试加载..."
+        # 加载驱动
+        modprobe uio
+        insmod /usr/src/linux-headers-$(uname -r)/extra/dpdk/igb_uio.ko
+        insmod /usr/lib/modules/$(uname -r)/extra/dpdk/rte_kni.ko carrier=on
+    fi
+}
+# dpdk网口绑定(内核驱动换igb_uio)  
+bind_nic() {
 ### 注：ens6 要替换为dpdk要绑定的网口名
-eth1_pci=$(/usr/local/bin/dpdk-devbind.py -s|grep "ens6"|awk '{print $1}')
-ifconfig ens6 down
-/usr/local/bin/dpdk-devbind.py -u ens6
-/usr/local/bin/dpdk-devbind.py -b igb_uio $eth1_pci
+    if /usr/local/bin/dpdk-devbind.py -s | grep drv=igb_uio; then
+        echo "已有网口绑定igb_uio"
+    else
+        echo "尝试将ens6口绑定到igb_uio"
+        local ens6_pci=$(/usr/local/bin/dpdk-devbind.py -s|egrep 'unused=[^,]*,igb_uio|ens6'|awk '{print $1}')
+        if ip a|grep ens6;then
+            ifconfig ens6 down
+            /usr/local/bin/dpdk-devbind.py -u ens6
+        fi
+        /usr/local/bin/dpdk-devbind.py -b igb_uio $ens6_pci
+    fi
+}
+
+set_hugepages
+insmod_dpdk_io
+bind_nic
