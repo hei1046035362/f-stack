b885f27d564ae9be4061bc279c721a8cf6a31413
hepeng <hepeng@teamgaga.com>
2025-08-12 15:27:27 +0800

    [fix]core_id换成进程序号

--- adapter/micro_thread/tgg/tgg_comm/tgg_master_timers.c
+++ adapter/micro_thread/tgg/tgg_comm/tgg_master_timers.c
@@ -170,15 +170,15 @@ static void update_gwrcv_secondary_heart_beat(struct rte_timer* tm, void* arg) {
     }
 }
 
-static int64_t s_max_concurency = 0;
+int64_t g_max_concurency = 0;
 static int64_t s_last_fd_count = 0;
 static void concurrency_stat(struct rte_timer* tm, void* arg)
 {
     int64_t cur_count = tgg_count_idx(g_core_id);
     int64_t concurency = cur_count - s_last_fd_count;
-    if(concurency > 0 && s_max_concurency < concurency) {// 最大并发
-        s_max_concurency = concurency;
-        LOG_WARNING("core[%d] max concurency :%ld", g_core_id, s_max_concurency);
+    if(concurency > 0 && g_max_concurency < concurency) {// 最大并发
+        g_max_concurency = concurency;
+        LOG_WARNING("core[%d] max concurency :%ld", g_core_id, g_max_concurency);
     }
     s_last_fd_count = cur_count;
 }
